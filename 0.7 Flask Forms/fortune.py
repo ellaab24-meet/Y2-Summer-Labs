95d6d38df1e7fcb46eec0d23ebbfb226d72b5fba
ellaab24-meet <ela2008.abrams@gmail.com>
2024-07-17 21:02:46 +0300

    Create fortune.py
    l

---
+++ 0.7 Flask Forms/fortune.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__ , template_folder='templates' , 
+static_folder='static')
+ 
+@app.route('/home' , methods = ['GET' , 'POST'])
+def home(): 
+    if request.method == 'GET':
+    return render_template("home.html")
+elif request.method == 'POST':
+    birth_month = request.form['birthMonth']
+    return redirect(url_for('fortune' , month = birth_month))
+    
+
+ 
+@app.route('/fortune')
+def fortune():
+    month = request.args.get('month', '')
+    month_langth = len(month)
+    if month_langth <= len(fortune):
+    fortunes = [
+    "You will have a great day today!" , 
+    "You'r most recent dream will come true!" , 
+    "Someone will suprise you with a kind gesture!" , 
+    "You will be the reason for someone's smile today!" , 
+    "Good news is on it's way!" , 
+    "If you work hard, it will pay off" , 
+    "You will find love in unexpected an place" , 
+    "Don't let this oppertunity pass you by!" , 
+    "What ever it is you're thinking of doing - don't!" , 
+    "Follow you'r heart!"]
+
+    fortNum = random.randint(0, len(fortunes))
+    return render_template("fortune.html" , fortune = fortunes[fortNum])
+
+else:
+    random_fortune = "your birth month is too long for a fortune"
+
+return render_template('fortune.html' , fortune = random_fortune)
+
+
+if __name__ == '__main__': 
+app.run(debug = True)
